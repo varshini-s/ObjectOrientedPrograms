7f40b2b0633e20ffd5549abb2f46001cf56210b3
varshini <dreamvarshini@gmail.com>
2021-09-05 22:13:52 +0530

    ADDED: function to print the report of stocks

--- src/com/bridgelabz/stockaccountmanagement/StockOperationsImpl.java
+++ src/com/bridgelabz/stockaccountmanagement/StockOperationsImpl.java
@@ -25,7 +25,23 @@ public class StockOperationsImpl implements StockOperationsIF
 		return totalStockValue;
 	}
 
-	
+	@Override
+	public void printReport(Stock[] stocksList) 
+	{
+		for(int index=0;index<stocksList.length;index++)
+		{
+			Stock stock=stocksList[index];
+			System.out.println("name of stock: "+stock.getName());
+			System.out.println("number of shares :"+stock.getNumberOfShares());
+			System.out.println("share price: "+stock.getSharePrice());
+			System.out.println("total stock value this stock: "+calculateStockValue(stock));
+			System.out.println("-------------------------------------------------------------");
+			
+		}
+		System.out.println("Total stock value of all stocks: "+calculateStockValue(stocksList));
+
+		
+	}
 
 	
 }
